acc4c54c929a32724055f0cb9b73c6bb5d7f0b1e
matthewrpacker <matthewrpacker@gmail.com>
2016-08-30 22:06:28 -0600

    Add Faraday functionality to PORO

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,35 +1,112 @@
 class GithubService
-  def initialize
-    @connection = Faraday.new("http://my-chaimz.herokuapp.com/api/v1/")
-    @connection.headers["Authorization"] = "Bearer 35148ad62db32ff044d6df2cd57"
+  attr_reader :current_user
+
+  def initialize(current_user)
+    @connection = Faraday.new "https://api.github.com/"
+    @connection.params["client_id"] = ENV['GITHUB_KEY']
+    @connection.params["client_secret"] = ENV['GITHUB_SECRET']
+    @current_user = current_user
   end
 
-  def get_users
-    # response = @connection.get "artists"
-    # parse(response.body)
-    response = @connection.get do |req|
-      req.url "users/#{id}"
-      # req.params['key'] = 'value'
-    end
-
-    parse(response.body)
+  def user_info
+    new_response = @connection.get("/users/#{current_user.nickname}")
+    JSON.parse(new_response.body)
   end
 
-  def get_user(id)
-    response = @connection.get "users/#{id}"
-    parse(response.body)
+  def login_name
+    user_info['login']
   end
 
-  def create_user(users_hash)
-    response = @connection.post("users?name=#{users_hash['name']}")
-    parse(response.body)
+  def avatar_url
+    user_info['avatar_url']
   end
-byebug
-  private
 
-  def parse(response)
-    JSON.parse(response, symbolize_names: true)
+  def repos_url
+    user_info['repos_url']
+  end
+
+  def public_repos_count
+    user_info['public_repos']
+  end
+
+  def gists_url
+    user_info['gists_url']
+  end
+
+  def public_gists_count
+    user_info['public_gists']
+  end
+
+  def followers
+    user_info['followers']
+  end
+
+  def following
+    user_info['following']
   end
 end
 
-GithubService.new
+
+# {"login"=>"matthewrpacker",
+#
+#  "id"=>14930925,
+#
+#  "avatar_url"=>"https://avatars.githubusercontent.com/u/14930925?v=3",
+#
+#  "gravatar_id"=>"",
+#
+#  "url"=>"https://api.github.com/users/matthewrpacker",
+#
+#  "html_url"=>"https://github.com/matthewrpacker",
+#
+#  "followers_url"=>"https://api.github.com/users/matthewrpacker/followers",
+#
+#  "following_url"=>
+#   "https://api.github.com/users/matthewrpacker/following{/other_user}",
+#
+#  "gists_url"=>"https://api.github.com/users/matthewrpacker/gists{/gist_id}",
+#
+#  "starred_url"=>
+#   "https://api.github.com/users/matthewrpacker/starred{/owner}{/repo}",
+#
+#  "subscriptions_url"=>
+#   "https://api.github.com/users/matthewrpacker/subscriptions",
+#
+#  "organizations_url"=>"https://api.github.com/users/matthewrpacker/orgs",
+#
+#  "repos_url"=>"https://api.github.com/users/matthewrpacker/repos",
+#
+#  "events_url"=>"https://api.github.com/users/matthewrpacker/events{/privacy}",
+#
+#  "received_events_url"=>
+#   "https://api.github.com/users/matthewrpacker/received_events",
+#
+#  "type"=>"User",
+#
+#  "site_admin"=>false,
+#
+#  "name"=>nil,
+#
+#  "company"=>nil,
+#
+#  "blog"=>nil,
+#
+#  "location"=>nil,
+#
+#  "email"=>nil,
+#
+#  "hireable"=>nil,
+#
+#  "bio"=>nil,
+#
+#  "public_repos"=>25,
+#
+#  "public_gists"=>3,
+#
+#  "followers"=>2,
+#
+#  "following"=>0,
+#
+#  "created_at"=>"2015-10-01T19:42:46Z",
+#
+#  "updated_at"=>"2016-08-30T13:53:35Z"}
