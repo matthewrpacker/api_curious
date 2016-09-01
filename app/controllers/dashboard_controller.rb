b13e62a4be8f26bb3a2d27ef1bc7ee258c1830ef
matthewrpacker <matthewrpacker@gmail.com>
2016-09-01 08:11:49 -0600

    Add UserInfo object

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
     @github = GithubService.new(current_user)
-    @github.user_info
+    @user_info = UserInfo.new(@github.user_info)
     @github.starred_info
     @github.repos_info
   end
